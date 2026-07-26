1070d35a3588bcf74582e51035fe4dbd1a5a0587
runmin <812716592@qq.com>
2026-07-26 12:38:41 +0800

    chore: bump version to v0.12

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-"""m3u8-dl-hls-gui v0.11 - CustomTkinter 桌面应用"""
+"""m3u8-dl-hls-gui v0.12 - CustomTkinter 桌面应用"""
 
 import os
 import sys
@@ -597,7 +597,7 @@ class App(ctk.CTk):
 
     def __init__(self):
         super().__init__()
-        self.title("m3u8-dl-hls-gui v0.11")
+        self.title("m3u8-dl-hls-gui v0.12")
         self.geometry("900x620")
         self.minsize(750, 500)
         self.configure(fg_color=COLORS["bg"])
