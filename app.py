c7feebfb3c634bafcf5b6a2bf411db8d3a7f3baa
runmin <812716592@qq.com>
2026-07-31 00:45:42 +0800

    fix: unify version string to v0.19

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-"""m3u8-dl-hls-gui v0.15 - CustomTkinter 桌面应用"""
+"""m3u8-dl-hls-gui v0.19 - CustomTkinter 桌面应用"""
 
 import os
 import sys
@@ -461,7 +461,7 @@ class App(ctk.CTk):
 
     def __init__(self):
         super().__init__()
-        self.title("m3u8-dl-hls-gui v0.17")
+        self.title("m3u8-dl-hls-gui v0.19")
         self.geometry("930x620")
         self.minsize(750, 500)
         self.configure(fg_color=COLORS["bg"])
